13d8457106228d1e871a6c93533d2d90c944d893
bodanapusaileela-gl <174320793+bodanapusaileela-gl@users.noreply.github.com>
2024-12-19 17:32:46 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = "~> 1.9.3"
+  required_version = "~> 1.9.3, < 2.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,7 +8,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baketleela"
+    bucket         = "leeladevopsaws.today"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
